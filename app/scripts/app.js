d86dad51e430b2838616ffd09cf3febe74ae35e2
Tim van der Lippe <tvanderlippe@gmail.com>
2016-02-23 22:24:16 +0100

    Fix error for undefined at onMenuSelect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,16 +9,18 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 (function(document, window) {
 'use strict';
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+  window.onload = function () {
+    // Grab a reference to our auto-binding template
+    // and give it some initial binding values
+    // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+    var app = document.querySelector('#app');
 
-  // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onMenuSelect = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
-      drawerPanel.closeDrawer();
-    }
+    // Close drawer after menu item is selected if drawerPanel is narrow
+    app.onMenuSelect = function() {
+      var drawerPanel = document.querySelector('#paperDrawerPanel');
+      if (drawerPanel.narrow) {
+        drawerPanel.closeDrawer();
+      }
+    };
   };
 })(document, window);
