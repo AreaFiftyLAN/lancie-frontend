189646e040c707cb47be8e24012e0f030da749f9
Martijn Janssen <martijn9612+github@gmail.com>
2017-09-15 20:47:12 +0200

    Correct dependency and require

--- gulp/project.js
+++ gulp/project.js
@@ -16,7 +16,7 @@ const gulpif = require('gulp-if');
 const mergeStream = require('merge-stream');
 const polymerBuild = require('polymer-build');
 const gulpreplace = require('gulp-replace');
-const uglify = require('gulp-babili');
+const uglify = require('gulp-babel-minify');
 const cssSlam = require('css-slam').gulp;
 const htmlMinifier = require('gulp-html-minifier');
 
