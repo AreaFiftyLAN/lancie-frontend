96e2ef9fff86231ea0563e68325b900e1deaa993
elarb <elarb@users.noreply.github.com>
2018-02-02 11:18:01 +0100

    set sw scope to rootPath

--- scripts/sw-register.js
+++ scripts/sw-register.js
@@ -1,6 +1,9 @@
-if ('serviceWorker' in navigator) {
+window.Polymer = {rootPath: '/'};
 
-  navigator.serviceWorker.register('service-worker.js').then((reg) => {
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: Polymer.rootPath,
+  }).then((reg) => {
       if (!navigator.serviceWorker.controller) {
         return;
       }
