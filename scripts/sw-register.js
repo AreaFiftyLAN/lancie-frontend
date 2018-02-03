732f07e2ee60f4b89a8e7a2569ce2fc0672bd978
e_lar <elarb@users.noreply.github.com>
2018-02-03 17:37:43 +0100

    Add service worker (#573)
    * Add service worker
    
    * Use <a> for reload, remove localhost filter, import sw-message.js
    
    * remove unnecessary chars
    
    * dispatchEvent instead of fire
    
    * don't display anchor in toast before paper-toast import
    
    * properly display toast, remove unnecessary message to sw
    
    * set sw scope to rootPath
    
    * Fix configuration issues in sw-precache-config.js
    
    * Fix warnings reported by polymer-analyzer
    
    * Recurse into images and cache fonts
    
    * remove slash from navigateFallback
    
    * Also cache data/**/*

---
+++ scripts/sw-register.js
@@ -0,0 +1,38 @@
+'use strict';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: Polymer.rootPath,
+  }).then((reg) => {
+      if (!navigator.serviceWorker.controller) {
+        return;
+      }
+
+      if (reg.waiting) {
+        updateReady();
+        return;
+      }
+
+      if (reg.installing) {
+        trackInstalling(reg.installing);
+        return;
+      }
+
+      reg.addEventListener('updatefound', () => {
+        trackInstalling(reg.installing);
+      });
+    }
+  );
+}
+
+let updateReady = () => {
+  document.dispatchEvent(new CustomEvent('update-toast'));
+};
+
+let trackInstalling = (worker) => {
+  worker.addEventListener('statechange', () => {
+    if (worker.state === 'installed') {
+      updateReady();
+    }
+  });
+};
---
+++ sw-precache-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  staticFileGlobs: [
+    'index.html',
+    'ce-fix.html',
+    'manifest.json',
+    'favicon.ico',
+    'favicon.jpg',
+    'data/**/*',
+    'images-optimized/**/*',
+    'scripts/**/*',
+    'bower_components/webcomponentsjs/webcomponents-loader.js'
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\/data\/.*.(json|md)/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'data-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /api\.areafiftylan.nl/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'api-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /fonts\.(gstatic|googleapis)\.com/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          name: 'api-cache'
+        }
+      }
+    },
+  ],
+  navigateFallback: 'index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+};
